96e64d483f4890c843e4523e3b8eec0276591ac7
sukina12 <sukina_89@yahoo.com>
2021-08-16 14:58:06 +0300

    editing

--- src/app.js
+++ src/app.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 import ToDo from './components/todo/todo.js';
 // import List from './components/List';
-import Header from './components/Header.js';
+import Header from './components/header/Header.js';
 // import Main from './components/Main.js';
-import ManageAppContextProvider from './components/todo/context/manageApp-context.js';
+import ManageAppContextProvider from './context/setting/context.js';
 
 export default class App extends React.Component {
   render() {
