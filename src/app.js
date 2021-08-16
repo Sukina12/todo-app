cd1f99d712547fde782ac867060c81780fa668a0
sukina12 <sukina_89@yahoo.com>
2021-08-16 08:16:11 +0300

    No Completed

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ToDo from './components/todo/todo.js';
 // import List from './components/List';
 import Header from './components/Header.js';
+// import Main from './components/Main.js';
 import ManageAppContextProvider from './components/todo/context/manageApp-context.js';
 
 export default class App extends React.Component {
@@ -14,6 +15,7 @@ export default class App extends React.Component {
           <ToDo />
           {/* <List/> */}
         </ManageAppContextProvider>
+        {/* <Main/> */}
       </React.Fragment>
     );
   }
---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Main extends Component {
+  render() {
+    return (
+      <h2>
+        Main Component !
+      </h2>
+    )
+  }
+}
+
+export default Main;
